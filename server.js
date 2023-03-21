49d563de1ed73034c55fdd69f696065dfb5457ad
Natan Yona <natanyona@gmail.com>
2023-03-21 19:00:11 -0300

    desafio-15

--- server.js
+++ server.js
@@ -300,7 +300,7 @@ io.on('connection', (socket) => {
 
 ///connection to mongodb compass database
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://localhost:27017/Users', {
+mongoose.connect('mongodb://127.0.0.1:27017/users', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
